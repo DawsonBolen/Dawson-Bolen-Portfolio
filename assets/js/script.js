98524db986c7fa776b24be4dd90c30c63f1a4950
DawsonBolen <dawson120@live.com>
2023-05-14 23:26:34 -0700

    got the x buttons working and added some content. I still need to add a paragraph somewhere explaining the drink campaign

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,107 +42,24 @@ var content = document.querySelectorAll('.web-projects-content ');
 
 for (i = 0; i < showMoreButtons.length; i++) {
   showMoreButtons[i].addEventListener("click", function(){
-    console.log(this);
     var content = this.closest(".web-project").children[1];
+    var parent = this.parentElement;
+    parent.classList.add('hide');
     content.classList.remove('hide');
   });
 }
 
 
-/*
-for (i = 0; i < showMoreButtons.length; i++) {
-  showMoreButtons[i].addEventListener("click", function(){
-    console.log(this);
-    var content = this.closest(".web-proj").children[1];
-    content.classList.remove('hide');
+var closeContentButtons = document.querySelectorAll('.x-btn');
+
+
+
+for (i = 0; i < closeContentButtons.length; i++) {
+  closeContentButtons[i].addEventListener("click", function(){
+    var openedContent = this.closest(".web-projects-content");
+    var showMoreAgain = this.closest(".web-project").children[0];
+    var showIt = showMoreAgain.children[0];
+    openedContent.classList.add('hide');
+    showIt.classList.remove('hide');
   });
 }
-
-/
-
-/*
-showMoreButtons.forEach(function (i) {
-  i.addEventListener("click", function(event){
-    let projectDescriptions = document.querySelectorAll('.web-projects-content');
-    var element = event.target;
-   
-
-
-    projectDescriptions.classList.remove('hide');
-   
-  });
-});
-*/
-
-/*
-
-var showMoreButtons = document.querySelectorAll('.read-more');
-var projectDescriptions = document.querySelectorAll('.web-projects-content');
-var y;
-for (i = 0; y < showMoreButtons.length; y ++) {
-  showMoreButtons[y].addEventListener("click", function(){
-    var elements = document.getElementsByClassName('').children
-  
-  });
-}
-
-function showDescription() {
- var descrip = 
-}
-
-
-*/
-/*
- for (let x = 0; x < projectDescriptions.length; x++) {
-    projectDescriptions[x].classList.remove('hide');
-  }
-
-  projectDescriptions[y].classList.remove('hide');
-*/
-
-
-/*
-todoList.addEventListener("click", function(event) {
-  var element = event.target;
-
-  // Checks if element is a button
-  if (element.matches("button") === true) {
-    // Get its data-index value and remove the todo element from the list
-    var index = element.parentElement.getAttribute("data-index");
-    todos.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
-  }
-});
-
-*/
-
-/*
-sitesAndApps.forEach(function (i) {
-  i.addEventListener("click", function(event){
-    var element = event.target;
-    if (element.matches("button") === true) {
-      var currentContent = element.parentElement.children;
-      currentContent[1].classList.remove('hide');
-    }
-  });
-});
-
-
-*/
-
-/*
-
-projectCards.addEventListener("click", function(event){
-  var element = event.target;
-  let currentContent = element.parentElement;
-  let theContent =currentContent.parentElement.children;
-  if (element.matches("button") === true) {
-    
-    theContent[1].classList.remove('hide');
-  }
-})
-
-*/
